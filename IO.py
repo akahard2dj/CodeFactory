f0faada6cf84ed1f9b6a1a85e136795b0d7ee7c4
akahard2dj <akahard2dj@naver.com>
2015-12-14 17:01:06 +0900

    Update IO.py

--- IO.py
+++ IO.py
@@ -1,16 +1,18 @@
 import json
 import time
 
-def writeJSON(fname, list):
+def writeJSON(fname, listItem):
     timestampStr = time.strftime("%Y-%m-%d-", time.localtime())
     fnameTimestamp = timestampStr + fname
-    j = json.dumps(list)
+    j = json.dumps(listItem)
     with open(fnameTimestamp,'w') as f:
         f.write(j)
     f.close()
 
 def loadJSON(fname):
-    with open(fname, 'r') as f:
-        list = json.load(f)
+    timestampStr = time.strftime("%Y-%m-%d-", time.localtime())
+    fnameTimestamp = timestampStr + fname
+    with open(fnameTimestamp, 'r') as f:
+        listItem = json.load(f)
 
-    return list
+    return listItem
