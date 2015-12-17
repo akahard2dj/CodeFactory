442bb297e25cc4bf3ec7f3b60715e204c3654bad
akahard2dj <akahard2dj@naver.com>
2015-12-17 16:32:13 +0900

    Update IO.py

--- IO.py
+++ IO.py
@@ -9,10 +9,23 @@ def writeJSON(fname, listItem):
         f.write(j)
     f.close()
 
+def staticWriteJSON(fname, listItem):
+    j = json.dumps(listItem)
+    with open(fname,'w') as f:
+        f.write(j)
+    f.close()
+
 def loadJSON(fname):
     timestampStr = time.strftime("%Y-%m-%d-", time.localtime())
     fnameTimestamp = timestampStr + fname
     with open(fnameTimestamp, 'r') as f:
+        listItem = json.load(f)
+
+    return listItem
+
+def staticLoadJSON(fname):
+
+    with open(fname, 'r') as f:
         listItem = json.load(f)
 
     return listItem
