97096422fec013078937507aff2ef087c10cd198
akahard2dj <akahard2dj@naver.com>
2016-01-08 17:08:48 +0900

    Update IO.py

--- IO.py
+++ IO.py
@@ -1,5 +1,17 @@
 import json
 import time
+import codecs
+
+def dumpPageSource(fname, str):
+    file = codecs.open(fname, "w", "utf-8")
+    file.write(str)
+    file.close()
+
+def loadPageSource(fname):
+    file = codecs.open(fname, "r", "utf-8")
+    str = file.read()
+
+    return str
 
 def writeJSON(fname, listItem):
     timestampStr = time.strftime("%Y-%m-%d-", time.localtime())
