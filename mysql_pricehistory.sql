28b3b7d0ecabbcef7261b7815b4e9d1077baec0c
akahard2dj <akahard2dj@naver.com>
2016-06-14 15:51:03 +0900

    Create mysql_pricehistory.sql

---
+++ mysql_pricehistory.sql
@@ -0,0 +1,13 @@
+create table pricehistory(
+id int not null auto_increment,
+tradeprice char(10),
+tradeyear char(5),
+c3namekr char(12),
+c4namekr char(100),
+trademonth char(3),
+tradeperiod char(6),
+tradearea char(10),
+address char(10),
+tradefloor char(3),
+c4code char(10),
+primary key(id));
