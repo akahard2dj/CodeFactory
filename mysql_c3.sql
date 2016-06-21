5a34b8a19f8ed37425ea74604a2ae60059917d81
akahard2dj <akahard2dj@naver.com>
2016-06-21 14:07:45 +0900

    Create mysql_c3.sql

---
+++ mysql_c3.sql
@@ -0,0 +1,11 @@
+create table c3(
+timestamp char(10),
+c1Code char(10),
+c1CoordX float(9,6),
+c1CoordY float(9,6),
+c1NameKR char(12),
+c2Code char(10),
+c2NameKR char(12),
+c3Code char(10),
+c3NameKR char(12),
+c3TotalCounts char(5));
